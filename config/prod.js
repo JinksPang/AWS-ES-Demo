80902b1f3803e9474a879f56314174d5761f1cd1
Jinks <jinks_pang@163.com>
2019-06-17 09:30:04 +0800

    Create prod.js

---
+++ config/prod.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var config = {
+    env: 'dev',
+    port: 3000,
+    debug: true,
+  
+    es_region : 'us-west-1',
+    es_domain : 'prod******.us-west-1.es.amazonaws.com',
+    es_index : 'es_index',
+    es_doc_type : 'es_type',
+    aws_access_key_id : "aws_access_key_id",
+    aws_secret_access_key : "aws_secret_access_key"
+};
+module.exports=config;
